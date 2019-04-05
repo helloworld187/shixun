b888416608c1ad9bf9ae5dd56f4e89ca66d7f9c1
zhangli <870629252@qq.com>
2019-04-05 10:03:02 +0800

    Material Design

--- material_design_application/src/main/java/com/zhangli/test/material_design_application/MainActivity.java
+++ material_design_application/src/main/java/com/zhangli/test/material_design_application/MainActivity.java
@@ -1,20 +1,48 @@
 package com.zhangli.test.material_design_application;
 
+import android.content.res.ColorStateList;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.TabHost.TabSpec;
+
+import com.zhangli.test.material_design_application.Frament.FocusFragment;
+import com.zhangli.test.material_design_application.Frament.MessageFragment;
+import com.zhangli.test.material_design_application.Frament.PeopleFragment;
+import com.zhangli.test.material_design_application.Frament.StarFragment;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
+
+    //定义FragmentTabHost对象
+    private FragmentTabHost mTabHost;
+
+    //定义一个布局
+    private LayoutInflater layoutInflater;
+
+    //定义数组来存放Fragment界面
+    private Class fragmentArray[] = {MessageFragment.class, PeopleFragment.class,FocusFragment.class, StarFragment.class};
+
+    //定义数组来存放导航图标
+    private int mImageViewArray[] = {R.animator.message,R.animator.people,R.animator.focus,R.animator.star};
+
+    //Tab选项卡的文字
+    private String mTextviewArray[] = {"消息", "联系人", "看点","动态"};
+
+    private ColorStateList colorStateList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.menu);
 
         }
-        navigationView.setCheckedItem(R.id.nav_person);
+        navigationView.setCheckedItem(R.id.member);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -45,6 +73,43 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+
+        //实例化布局对象
+        layoutInflater = LayoutInflater.from(this);
+
+        //实例化TabHost对象，得到TabHost
+        mTabHost = (FragmentTabHost)findViewById(R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.content);
+
+        //得到fragment的个数
+        int count = fragmentArray.length;
+
+        for(int i = 0; i < count; i++){
+        //为每一个Tab按钮设置图标、文字和内容
+            TabSpec tabSpec = mTabHost.newTabSpec(mTextviewArray[i]).setIndicator(getTabItemView(i));
+        //将Tab按钮添加进Tab选项卡中
+            mTabHost.addTab(tabSpec, fragmentArray[i], null);
+        }
+    }
+    /**
+     * 给Tab按钮设置图标和文字
+     */
+    private View getTabItemView(int index){
+        View view = layoutInflater.inflate(R.layout.tabhost_item, null);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
+        imageView.setImageResource(mImageViewArray[index]);
+
+        if(colorStateList == null){
+            colorStateList = getResources().getColorStateList(R.color.change_color_text, null);
+
+        }
+
+        TextView textView = (TextView) view.findViewById(R.id.textview);
+        textView.setText(mTextviewArray[index]);
+        textView.setTextColor(colorStateList);
+        return view;
     }
 
     @Override
